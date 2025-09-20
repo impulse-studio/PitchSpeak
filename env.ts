4314b778dfb5a4d1f49f59bca33f7b72e6b27281
Perez <perezpablopro91@gmail.com>
2025-09-20 17:56:40 +0200

    fix: env

--- env.ts
+++ env.ts
@@ -3,13 +3,9 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // DATABASE_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
     RESEND_FROM_EMAIL: z.string().min(1),
-
-    KV_REST_API_URL: z.string().min(1),
-    KV_REST_API_TOKEN: z.string().min(1),
 
     NODE_ENV: z.enum(["development", "production"]).optional(),
     VERCEL_URL: z.string().optional(),
